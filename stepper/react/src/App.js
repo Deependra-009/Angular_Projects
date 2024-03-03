ef5e249b9ad50c40a54643324d4c01fc8da754be
Deependra-009 <deependra04trivedi@gmail.com>
2024-03-03 16:48:11 +0530

    stepper angular-react

--- stepper/react/src/App.js
+++ stepper/react/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
@@ -10,32 +9,32 @@ function App() {
 
   return (
     <>
-      <div class="w-[100%] p-10 flex justify-center">
-        <div class="w-[80%]">
+      <div className="w-[100%] p-10 flex justify-center">
+        <div className="w-[80%]">
           {/* heading */}
           <div
-            class="w-[100%] text-[25px] text-red-500 font-semibold flex justify-center items-center"
+            className="w-[100%] text-[25px] text-red-500 font-semibold flex justify-center items-center"
           >
-            {/* Stepper */}
+            Stepper
           </div>
-          <div class="w-[100%] py-10">
+          <div className="w-[100%] py-10">
 
             <ol
-              class="flex items-center w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base"
+              className="flex items-center w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base"
             >
 
 
               <li
-                class="flex md:w-full items-center sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700"
+                className="flex md:w-full items-center sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700"
               >
                 <span
-                  className={`${step == 1 ? 'text-blue-600' : 'text-green-600'} flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500`}
+                  className={`${step === 1 ? 'text-blue-600' : 'text-green-600'} flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500`}
 
                 >
                   {
-                    (step != 1) &&
+                    (step !== 1) &&
                     <svg
-                      class="w-3.5 h-3.5 sm:w-4 sm:h-4 me-2.5"
+                      className="w-3.5 h-3.5 sm:w-4 sm:h-4 me-2.5"
                       aria-hidden="true"
                       xmlns="http://www.w3.org/2000/svg"
                       fill="currentColor"
@@ -47,19 +46,19 @@ function App() {
                     </svg>
                   }
                   {
-                    (step == 1) &&
-                    <span class="me-2">1</span>
+                    (step === 1) &&
+                    <span className="me-2">1</span>
                   }
-                  Personal <span class="hidden sm:inline-flex sm:ms-2">Info</span>
+                  Personal <span className="hidden sm:inline-flex sm:ms-2">Info</span>
                 </span>
               </li>
 
 
               <li
-                class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700"
+                className="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700"
               >
                 <span
-                  className={`${step == 2
+                  className={`${step === 2
                     ? 'text-blue-600'
                     : step < 2
                       ? 'text-gray-600'
@@ -68,9 +67,9 @@ function App() {
                 >
 
                   {
-                    (step != 2) &&
+                    (step !== 2) &&
                     <svg
-                      class="w-3.5 h-3.5 sm:w-4 sm:h-4 me-2.5"
+                      className="w-3.5 h-3.5 sm:w-4 sm:h-4 me-2.5"
                       aria-hidden="true"
                       xmlns="http://www.w3.org/2000/svg"
                       fill="currentColor"
@@ -82,15 +81,15 @@ function App() {
                     </svg>
                   }
                   {
-                    (step == 2) &&
-                    <span class="me-2">2</span>
+                    (step === 2) &&
+                    <span className="me-2">2</span>
                   }
-                  Account <span class="hidden sm:inline-flex sm:ms-2">Info</span>
+                  Account <span className="hidden sm:inline-flex sm:ms-2">Info</span>
                 </span>
               </li>
-              <li class="flex items-center">
+              <li className="flex items-center">
                 <span
-                  className={`${step == 3
+                  className={`${step === 3
                     ? 'text-blue-600'
                     : step < 3
                       ? 'text-gray-600'
@@ -98,9 +97,9 @@ function App() {
 
                 >
                   {
-                    (step != 3) &&
+                    (step !== 3) &&
                     <svg
-                      class="w-3.5 h-3.5 sm:w-4 sm:h-4 me-2.5"
+                      className="w-3.5 h-3.5 sm:w-4 sm:h-4 me-2.5"
                       aria-hidden="true"
                       xmlns="http://www.w3.org/2000/svg"
                       fill="currentColor"
@@ -112,18 +111,18 @@ function App() {
                     </svg>
                   }
                   {
-                    (step == 3) &&
-                    <span class="me-2">3</span>
+                    (step === 3) &&
+                    <span className="me-2">3</span>
                   } Confirmation
                 </span>
               </li>
             </ol>
 
 
-            {(step == 1) && <div class="w-[100%] py-10 text-[20px]">Step 1 (Personal Info)</div>}
-            {(step == 2) && <div class="w-[100%] py-10 text-[20px]">Step 2 (Account Info)</div>}
-            {(step == 3) && <div class="w-[100%] py-10 text-[20px]">Step 3 (Confirmation)</div>}
-            {(step == 4) && <div class="w-[100%] py-10 text-[20px]">Step 4 (Done)</div>}
+            {(step === 1) && <div className="w-[100%] py-10 text-[20px]">Step 1 (Personal Info)</div>}
+            {(step === 2) && <div className="w-[100%] py-10 text-[20px]">Step 2 (Account Info)</div>}
+            {(step === 3) && <div className="w-[100%] py-10 text-[20px]">Step 3 (Confirmation)</div>}
+            {(step === 4) && <div className="w-[100%] py-10 text-[20px]">Step 4 (Done)</div>}
 
 
 
@@ -131,23 +130,23 @@ function App() {
 
             {/* buttons */}
 
-            <div class="w-[100%] mt-10 flex justify-between">
+            <div className="w-[100%] mt-10 flex justify-between">
               {/* back button */}
-              <div class="w-[50%] flex justify-center items-center">
+              <div className="w-[50%] flex justify-center items-center">
                 <button
-                  className={`border-2 py-2 px-5 w-[30%]  ${step == 1 ? 'border-gray-400 text-gray-400' : 'border-black text-black'}`}
+                  className={`border-2 py-2 px-5 w-[30%]  ${step === 1 ? 'border-gray-400 text-gray-400' : 'border-black text-black'}`}
                   onClick={() => setStep(step - 1)}
-                  disabled={step == 1}
+                  disabled={step === 1}
 
                 >
                   BACK
                 </button>
               </div>
               {/* next button */}
-              <div class="w-[50%] flex justify-center items-center">
+              <div className="w-[50%] flex justify-center items-center">
                 <button
-                  className={`border-2 py-2 px-5 w-[30%] border-black ${step == 4 ? 'border-gray-400 text-gray-400' : 'border-black text-black'}`}
-                  disabled={step == 4}
+                  className={`border-2 py-2 px-5 w-[30%] border-black ${step === 4 ? 'border-gray-400 text-gray-400' : 'border-black text-black'}`}
+                  disabled={step === 4}
                   onClick={() => setStep(step + 1)}
                 >
                   NEXT
